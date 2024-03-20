8570bfe54ac03ae75c0902995852181487fbe63f
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2024-03-20 19:43:40 -0400

    Got the index_html.h to work for S3

---
+++ Edits/index_html.h
@@ -0,0 +1,16 @@
+const char PAGE_MAIN[] PROGMEM = R"=====(
+<html lang="en" class="js-focus-visible">
+  <head>
+    <meta http-equiv='refresh' content='5' />
+    <title>HWSET Wind Turbine Control Panel</title>
+    <style>
+      body { background-color: #e6fffa; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }
+    </style>
+  </head>
+
+  <body>
+    <h1>HWSET Wind Turbine Control Panel</h1>
+    <p>Uptime: %02d:%02d:%02d</p>
+    <img src="/test.svg" />
+  </body>
+</html>)=====";
