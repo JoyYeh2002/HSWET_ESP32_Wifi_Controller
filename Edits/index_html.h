45949a853240f6f711b0e518503845382b5ff26e
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2024-04-03 21:26:34 -0400

    Subplots work

--- Edits/index_html.h
+++ Edits/index_html.h
@@ -9,183 +9,6 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
 <title>HWSET WIFI Turbine Dashboard</title>
 
 <style>
-  table {
-    position: relative;
-    width: 100%;
-    border-spacing: 0px;
-  }
-
-  tr {
-    border: 1px solid white;
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 20px;
-  }
-
-  th {
-    height: 20px;
-    padding: 3px 15px;
-    background-color: #343a40;
-    color: #FFFFFF !important;
-  }
-
-  td {
-    height: 20px;
-    padding: 3px 15px;
-  }
-
-  .tabledata {
-    font-size: 24px;
-    position: relative;
-    padding-left: 5px;
-    padding-top: 5px;
-    height: 25px;
-    border-radius: 5px;
-    color: #FFFFFF;
-    line-height: 20px;
-    transition: all 200ms ease-in-out;
-    background-color: #00AA00;
-  }
-
-  .fanrpmslider {
-    width: 30%;
-    height: 55px;
-    outline: none;
-    height: 25px;
-  }
-
-  .bodytext {
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 24px;
-    text-align: left;
-    font-weight: light;
-    border-radius: 5px;
-    display: inline;
-  }
-
-  .navbar {
-    width: 100%;
-    height: 50px;
-    margin: 0;
-    padding: 10px 0px;
-    background-color: #FFF;
-    color: #000000;
-    border-bottom: 5px solid #293578;
-  }
-
-  .fixed-top {
-    position: fixed;
-    top: 0;
-    right: 0;
-    left: 0;
-    z-index: 1030;
-  }
-
-  .navtitle {
-    float: left;
-    height: 50px;
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 50px;
-    font-weight: bold;
-    line-height: 50px;
-    padding-left: 20px;
-  }
-
-  .navheading {
-    position: fixed;
-    left: 60%;
-    height: 50px;
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-    line-height: 20px;
-    padding-right: 20px;
-  }
-
-  .navdata {
-    justify-content: flex-end;
-    position: fixed;
-    left: 70%;
-    height: 50px;
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-    line-height: 20px;
-    padding-right: 20px;
-  }
-
-  .category {
-    font-family: "Verdana", "Arial", sans-serif;
-    font-weight: bold;
-    font-size: 32px;
-    line-height: 50px;
-    padding: 20px 10px 0px 10px;
-    color: #000000;
-  }
-
-  .heading {
-    font-family: "Verdana", "Arial", sans-serif;
-    font-weight: normal;
-    font-size: 28px;
-    text-align: left;
-  }
-
-  .btn {
-    background-color: #444444;
-    border: none;
-    color: white;
-    padding: 10px 20px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-  }
-
-  .foot {
-    font-family: "Verdana", "Arial", sans-serif;
-    font-size: 20px;
-    position: relative;
-    height: 30px;
-    text-align: center;
-    color: #AAAAAA;
-    line-height: 20px;
-  }
-
-  .container {
-    max-width: 1800px;
-    margin: 0 auto;
-  }
-
-  table tr:first-child th:first-child {
-    border-top-left-radius: 5px;
-  }
-
-  table tr:first-child th:last-child {
-    border-top-right-radius: 5px;
-  }
-
-  table tr:last-child td:first-child {
-    border-bottom-left-radius: 5px;
-  }
-
-  table tr:last-child td:last-child {
-    border-bottom-right-radius: 5px;
-  }
-
-  /* Additional styles for the real-time plot */
-  #realTimePlotContainer {
-    width: 80%;
-    margin: 0 auto;
-    padding-top: 50px;
-  }
-
-  #realTimePlotCanvas {
-    width: 100%;
-    height: 400px;
-    /* Adjust height as needed */
-  }
-
 
   /* NEW STYLE STARTS HEREEEEEE */
   /* Reset some default styles */
@@ -251,7 +74,7 @@ section {
     overflow: hidden; /* Hide any overflow content */
   }
 
-  .rpm-chart, .pwr-chart, .wind-speed-chart{
+  .chart{
     width: 100%;
     height: 100%; /* Use 100% height to fill the parent container */
   }
@@ -340,17 +163,10 @@ section {
     background-color: #fac8c8;
 }
 
-.chart-container {
-  width: 100%;
-  height: 22vh;
-  background-color: #f0f0f0;
-  margin-bottom: 10px;
-}
 
 .chart {
   width: 100%;
-  height: 100%;
-  background-color: #e0e0e0;
+  height: 100%; /* Fill the entire height of the chart container */
 }
 
 .controls {
@@ -472,33 +288,6 @@ footer {
 
 <!-- NORMAL SENSOR MONITOR -->
 <body style="background-color: #efefef" onload="process()">
-  <header>
-    <div class="navbar fixed-top">
-     <!-- could put fixed title? not needed-->
-    </div>
-  </header>
-
-  <main class="container" style="margin-top:70px">
-  
-    <div class="category">Sensor Controls</div>
-  <div id="b0"></div>
-  <div id="v0"></div>
-  <div id="b1"></div>
-  <div id="v1"></div>
-  <div id="switch"></div>
-
-    <div class="bodytext">Switch</div>
-    <button type="button" class="btn" id="btn1" onclick="ButtonPress1()">Toggle</button>
-    </div>
-    <br>
-    <br>
-    <div class="bodytext">Fan Speed Control (RPM: <span id="fanrpm"></span>) </div>
-    <br>
-    <input type="range" class="fanrpmslider" min="0" max="255" value="0" width="0%"
-      oninput="UpdateSlider(this.value)" />
-    <br>
-    <br>
-  </main>
 
   <!-- MY HTML STARTS HERE -->
   <header>
@@ -519,44 +308,8 @@ footer {
     -->
     <section class="data-charts">
       <div class="chart-box">
-        <div class="data-box">
-            <h3>Wind Speed: </h3>
-            <p id="wind-speed-value" class="data-value">0 m/s</p>
-        </div>
-        <div class="chart-container">
-          <div id="wind-speed-chart" class="chart"></div>
-        </div>
+          <div id="charts" class="chart"></div>
       </div>
-
-      <div class="chart-box">
-        <div class="data-box">
-            <h3>RPM: </h3>
-            <p id="rpm-value" class="data-value">0</p>
-        </div>
-        <div class="chart-container">
-          <div id="rpm-chart" class="chart"></div>
-        </div>
-      </div>
-
-      <div class="chart-box">
-        <div class="data-box">
-            <h3>Output PWR: </h3>
-            <p id="pwr-value" class="data-value">0 W</p>
-        </div>
-        
-        <div class="chart-container">
-          <div id="pwr-chart" class="chart"></div> 
-        </div>
-      </div>
-
-      <!-- Put 2 state displays here -->
-
-      <!-- Tags: 
-      text: 
-        safety-state, backup-pwr-state
-      JS update: 
-        toggle-state
-      -->
 
       <div class="left-display-box">
         <div class="data-display-box">
@@ -577,7 +330,6 @@ footer {
         radio-button-msg, load-current-msg, blade-pitch-msg
     -->
     <section class="controls">
-
       <div class="control-box">
         <div class="input-btn-flex">
             <h3>Operation Mode </h3>
@@ -613,7 +365,6 @@ footer {
   <p id="load-current-msg" class="msg">CONFIG: Please enter load current.</p>
 </div>
 
-
 <div class="control-box">
   <div class="input-btn-flex">
     <h3>Blade Pitch (0 to 5):</h3>
@@ -629,21 +380,15 @@ footer {
       <p id="test-duration", class="msg">Test Duration: 0 mins, 0 sec</p>
     </div>
 
+    <p id="wind-speed-value">0</p>
+    <p id="rpm-value">0</p>
+    <p id="pwr-value">0</p>
+
     <button class="download-btn">Download Data Report</button>
     <p class="msg">SUCCESS: Data report downloaded at 23:32:41 on 03/31/2024.</p>
 
     </section>
   </main>
-
-  <!-- Tags: 
-    time-stamp, test-duration 
-class = msg -->
-  <footer>
-   <p> Version 1.0.0.</p>
-  </footer>
-
-  <footer div class="foot" id="temp">ESP32 Web Page Creation and Data Update Demo </div>
-  </footer>
 </body>
 
 <script type="text/javascript">
@@ -655,7 +400,6 @@ class = msg -->
 
   var time = new Date();
   
-  // Define 2 traces
   var trace1 = {
     x: [],
     y: [],
@@ -663,32 +407,137 @@ class = msg -->
     line: {
       color: '#80CAF6',
       shape: 'spline'
-    },
-    name: 'A0 Voltage'
+    }
   }
+  
+  var trace2 = {
+    x: [],
+    y: [],
+    xaxis: 'x2',
+    yaxis: 'y2',
+    mode: 'lines',
+    line: {color: '#DF56F1'}
+  };
+  
+  var trace3 = {
+    x: [],
+    y: [],
+    xaxis: 'x3',
+    yaxis: 'y3',
+    mode: 'lines',
+    line: {color: '#9a9a9a'}
+  };
+  
 
-  // Make sub-plot layout
+  var vw = window.innerWidth * 0.55;
+  var vh = 550; // put 3 plots together
+
+  // Define scaling constant for subplot height and spacing
+  var plotHeight = 0.22; // Adjust for desired subplot height (0 to 1)
+  var plotSpacing = 0.1; 
+
   var layout = {
-    xaxis: { // The top x axis
-      type: 'date',
+    width: vw,
+    height: vh,
+    margin: {
+    t: 0,  // top margin
+    b: 40, // bottom margin
+    l: 35, // left margin
+    r: 10  // right margin
+    },
+
+    xaxis: { // WIND SPEED
+      type: 'date', 
+      tickformat: '%H:%M:%S', // Set the tick format for time values
+      tickmode: 'linear', // Use linear tick mode for consistent tick spacing
+      tick0: new Date().getTime(), // Start the ticks from the current time
+      dtick: 5000, // Set the tick interval to 5s
+      showticklabels: true, // Show tick labels
       domain: [0, 1]
     },
-    yaxis: { 
-      domain: [0.6, 1],
-      range: [0, 3.3],
-      tickformat: '.2f'
-     }
+    yaxis: {
+      domain: [plotHeight * 2 + plotSpacing * 2, plotHeight * 3 + plotSpacing * 2], // 0.5, 0.7
+      range: [0, 3.4],
+      tickformat: '.2f',
+      tickmode: 'array',
+      tickvals: [0, 1, 3, 3.3]
+    },
+
+    xaxis2: { // RPM
+      type: 'date', 
+      anchor: 'y2', 
+      tickformat: '%H:%M:%S', // Set the tick format for time values
+      tickmode: 'linear', // Use linear tick mode for consistent tick spacing
+      tick0: new Date().getTime(), // Start the ticks from the current time
+      dtick: 5000, // Set the tick interval to 5s
+      showticklabels: true, // Show tick labe
+      domain: [0, 1]
+    },
+    yaxis2: {
+      anchor: 'x2', 
+      domain: [plotHeight + plotSpacing, plotHeight * 2 + plotSpacing], //0.25, 0.45 P2
+      range: [0, 510],
+      tickmode: 'array',
+      tickvals: [0, 100, 300, 500]
+    },
+
+    xaxis3: { // PWR
+      type: 'date', 
+      anchor: 'y3', 
+      tickformat: '%H:%M:%S', // Set the tick format for time values
+      tickmode: 'linear', // Use linear tick mode for consistent tick spacing
+      tick0: new Date().getTime(), // Start the ticks from the current time
+      dtick: 5000, // Set the tick interval to 5s
+      showticklabels: true, // Show tick labe
+      domain: [0, 1]
+    },
+    yaxis3: {
+      anchor: 'x3', 
+      domain: [0, plotHeight],
+      range: [0, 11],
+      tickmode: 'array',
+      tickvals: [0, 2, 4, 6, 8, 10]
+    }
   }
 
-  // Create the canvas
-  var data = [trace1];
-  Plotly.newPlot('wind-speed-chart', data, layout, { displayModeBar: false });
-  Plotly.newPlot('rpm-chart', data, layout, { displayModeBar: false });
-  Plotly.newPlot('pwr-chart', data, layout, { displayModeBar: false });
   
-  // --------------------------------------------
+  var ws_plot_title = {
+    x: 0.5, 
+    y: plotHeight * 3 + plotSpacing * 2,
+    xref: 'paper', 
+    yref: 'paper',
+    text: "Wind Speed: " + document.getElementById("wind-speed-value").textContent + "m/s",
+    showarrow: false // Hide arrow for cleaner look
+  };
 
+    var rpm_plot_title = {
+    x: 0.5, 
+    y: plotHeight * 2 + plotSpacing,
+    xref: 'paper', 
+    yref: 'paper',
+    text: "RPM: " + document.getElementById("rpm-value").textContent,
+    showarrow: false // Hide arrow for cleaner look
+  };
+
+  var pwr_plot_title = {
+    x: 0.5, 
+    y: plotHeight,
+    xref: 'paper',
+    yref: 'paper',
+    text: "OUTPUT POWER: " + document.getElementById("pwr-value").textContent + "W",
+    showarrow: false // Hide arrow for cleaner look
+  };
+  
+  // Add annotation to layout
+  layout.annotations = [pwr_plot_title, rpm_plot_title, ws_plot_title];
+
+  var data = [trace1,trace2,trace3]; 
+  
+  // Plotly.plot('charts', data, layout);  
+  Plotly.newPlot('charts', data, layout, { displayModeBar: false });
+  
   var cnt = 0;
+  
 
   // global variable visible to all java functions
   var xmlHttp = createXmlHttpObject();
@@ -703,27 +552,10 @@ class = msg -->
     return xmlHttp;
   }
 
+  /* This is E-Stop */
   function ButtonPress0() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("PUT", "BUTTON_0", false);
-    xhttp.send();
-  }
-
-  function ButtonPress1() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "BUTTON_1", false);
-    xhttp.send();
-  }
-
-  function UpdateSlider(value) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        // update the web based on reply from  ESP
-        document.getElementById("fanrpm").innerHTML = this.responseText;
-      }
-    }
-    xhttp.open("PUT", "UPDATE_SLIDER?VALUE=" + value, true);
     xhttp.send();
   }
 
@@ -731,12 +563,11 @@ class = msg -->
   function response() {
 
     // I want "plot-stream" to update here, so that it can plot a random number every second (when var dt updates)
-    var msg_B0;
     var msg_B1;
     var msg_V0;
     var msg_V1;
 
-    var msg;
+    var msg; // stop light
 
     var barwidth;
     var currentsensor;
@@ -753,87 +584,62 @@ class = msg -->
     formattedDate = dt.toLocaleDateString();
     document.getElementById("date-time").textContent = "Time: " + formattedDate + " " + formattedTime;
     
-    // B0
-    xmldoc = xmlResponse.getElementsByTagName("B0"); //bits for A0
-    msg_B0 = xmldoc[0].firstChild.nodeValue;
-    if (msg_B0 > 2048) {
+    // V0
+    xmldoc = xmlResponse.getElementsByTagName("wind-speed");  
+    msg_V0 = xmldoc[0].firstChild.nodeValue;
+
+   
+
+    if (msg_V0 > 0.5) {
       color = "#aa0000";
     } else {
       color = "#0000aa";
     }
-    barwidth = msg_B0 / 40.95;
-    document.getElementById("b0").innerHTML = msg_B0;
-    document.getElementById("b0").style.width = (barwidth + "%");
 
-    // V0
-    xmldoc = xmlResponse.getElementsByTagName("V0"); //volts for A0
-    msg_V0 = xmldoc[0].firstChild.nodeValue;
-    document.getElementById("v0").innerHTML = msg_V0;
     document.getElementById("wind-speed-value").textContent = msg_V0 + " m/s";
     document.getElementById("wind-speed-value").style.color = color;
-
-    document.getElementById("v0").style.width = (barwidth + "%");
-    document.getElementById("v0").style.backgroundColor = color;
-
+    
     // B1
-    xmldoc = xmlResponse.getElementsByTagName("B1");
+    xmldoc = xmlResponse.getElementsByTagName("rpm");
     msg_B1 = xmldoc[0].firstChild.nodeValue;
-    if (msg_B1 > 2048) {
-      color = "#aa0000";
-    } else {
-      color = "#0000aa";
-    }
-    document.getElementById("b1").innerHTML = msg_B1;
+
     document.getElementById("rpm-value").textContent = msg_B1;
     document.getElementById("rpm-value").style.color = color;
 
-
-    width = msg_B1 / 40.95;
-    document.getElementById("b1").style.width = (width + "%");
-    document.getElementById("b1").style.backgroundColor = color;
-
     // V1
-    xmldoc = xmlResponse.getElementsByTagName("V1");
+    xmldoc = xmlResponse.getElementsByTagName("pwr");
     msg_V1 = xmldoc[0].firstChild.nodeValue;
-    document.getElementById("v1").innerHTML = msg_V1;
     document.getElementById("pwr-value").textContent = msg_V1 + " W";
-
-    document.getElementById("v1").style.width = (width + "%");
-    document.getElementById("v1").style.backgroundColor = color;
-
     document.getElementById("pwr-value").style.color = color;
 
 
     xmldoc = xmlResponse.getElementsByTagName("LED");
     msg = xmldoc[0].firstChild.nodeValue;
+
+    // Red LED on pin 13 also toggles
     if (msg == 0) {
-      document.getElementById("btn0").textContent = "Turn ON";
+      document.getElementById("btn0").textContent = "STOP";
+      document.getElementById("btn0").style.backgroundColor = "#a2a2a2";
     } else {
-      document.getElementById("btn0").textContent = "Turn OFF";
+      document.getElementById("btn0").textContent = "STOPPED";
+      document.getElementById("btn0").style.backgroundColor = "#e74c3c";
     }
-    xmldoc = xmlResponse.getElementsByTagName("SWITCH");
-    msg = xmldoc[0].firstChild.nodeValue;
-    document.getElementById("switch").style.backgroundColor = "rgb(200,200,200)";
 
     // Plots: Update
-    var x_axis_interval = 100;
-    var update = {
-      x: [dt],
-      y: [msg_V0]
-    }
-    Plotly.extendTraces('wind-speed-chart', update, [0, 1], x_axis_interval)
-    Plotly.extendTraces('rpm-chart', update, [0, 1], x_axis_interval)
-    Plotly.extendTraces('pwr-chart', update, [0, 1], x_axis_interval)
+    var x_axis_interval = 200;
 
+    var update = {
+      x: [[dt], [dt], [dt]],
+      y: [[rand() * 3.3], [rand() * 500], [rand() * 10]]
+    }
+    
+    Plotly.extendTraces('charts', update, [0,1,2], x_axis_interval)
+   
     // update the text in the table
     if (msg == 0) {
-      document.getElementById("switch").innerHTML = "Switch is OFF";
       document.getElementById("btn1").innerHTML = "Turn ON";
-      document.getElementById("switch").style.color = "#0000AA";
     } else {
-      document.getElementById("switch").innerHTML = "Switch is ON";
       document.getElementById("btn1").innerHTML = "Turn OFF";
-      document.getElementById("switch").style.color = "#00AA00";
     }
 
   }
@@ -844,7 +650,7 @@ class = msg -->
       xmlHttp.onreadystatechange = response;
       xmlHttp.send(null);
     }
-    setTimeout("process()", 200);
+    setTimeout("process()", 400);
   }
 
   // MY JS STARTS HERE ---------------------------------------
